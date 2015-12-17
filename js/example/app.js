d4ed544f002c44a6d1815e2b3812feb67d7dfcae
memcaheon <memcaheon@sina.cn>
2015-12-17 13:43:07 +0800

    增加实例

---
+++ js/example/app.js
@@ -0,0 +1,14 @@
+var PSD = require('../../coffee/psd.coffee');
+/*var psd = PSD.fromFile('./mobile_demo.psd');
+psd.parse();
+
+console.log(psd.tree().export());
+console.log(psd.tree().childrenAtPath('A/B/C')[0].export());*/
+
+// You can also use promises syntax for opening and parsing
+PSD.open('./mobile_demo.psd').then(function (psd) {
+	//return psd.image.saveAsPng('./output.png');
+	console.log(psd);
+}).then(function () {
+	console.log('Finished!');
+});
